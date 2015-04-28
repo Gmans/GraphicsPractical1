c5e36573b855ec5a4db6f2629630e26946160c9c
Gmans <yoni_groosman@hotmail.com>
2015-04-28 10:53:26 +0200

    commit 2 t/m chapter 4

--- GraphicsPractical1/GraphicsPractical1/Game1.cs
+++ GraphicsPractical1/GraphicsPractical1/Game1.cs
@@ -11,81 +11,94 @@ using Microsoft.Xna.Framework.Media;
 
 namespace GraphicsPractical1
 {
-    /// <summary>
-    /// This is the main type for your game
-    /// </summary>
     public class Game1 : Microsoft.Xna.Framework.Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        private FrameRateCounter frameRateCounter;
+        private BasicEffect effect;
+        private VertexPositionColor[] vertices;
+        private Camera camera;
+        private float angle;
 
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+            this.frameRateCounter = new FrameRateCounter(this);
+            this.Components.Add(this.frameRateCounter);
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
+            this.graphics.PreferredBackBufferWidth = 800;
+            this.graphics.PreferredBackBufferHeight = 600;
+            this.graphics.IsFullScreen = false;
+            this.graphics.SynchronizeWithVerticalRetrace = false;
+            this.graphics.ApplyChanges();
+            this.IsFixedTimeStep = false;
             base.Initialize();
         }
-
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
+            this.effect = new BasicEffect(this.GraphicsDevice);
+            this.setupVertices();
+            this.effect.VertexColorEnabled = true;
+            this.camera = new Camera(new Vector3(0, 0, 50), new Vector3(0, 0, 0), new Vector3(0, 1, 0));
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// all content.
-        /// </summary>
+
         protected override void UnloadContent()
         {
-            // TODO: Unload any non ContentManager content here
+ 
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            // Allows the game to exit
+            float timeStep = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            this.Window.Title = "Graphics Tutorial | FPS: " + this.frameRateCounter.FrameRate;
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
                 this.Exit();
-
-            // TODO: Add your update logic here
+            this.angle += timeStep * 3.0f;
 
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            // TODO: Add your drawing code here
-
+            this.GraphicsDevice.RasterizerState = new RasterizerState
+            {
+                CullMode = CullMode.None
+            };
+            GraphicsDevice.Clear(Color.DarkSlateBlue);
+            this.effect.Projection = this.camera.ProjectionMatrix;
+            this.effect.View = this.camera.ViewMatrix;
+            Vector3 rotAxis = new Vector3(3 * this.angle, this.angle, 2 * this.angle);
+            rotAxis.Normalize();
+            Matrix translation = Matrix.CreateTranslation(-20.0f / 3.0f, -10.0f / 3.0f, 0);
+            Matrix rotation = Matrix.CreateFromAxisAngle(rotAxis, this.angle);
+            this.effect.World = translation * rotation;
+            foreach (EffectPass pass in this.effect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                this.GraphicsDevice.DrawUserPrimitives(PrimitiveType.TriangleList, this.vertices, 0, 1, VertexPositionColor.VertexDeclaration);
+            }
             base.Draw(gameTime);
+        }
+
+        private void setupVertices()
+        {
+            this.vertices = new VertexPositionColor[3];
+
+            this.vertices[0].Position = new Vector3(0f, 0f, 0f);
+            this.vertices[0].Color = Color.Red;
+            this.vertices[1].Position = new Vector3(10f, 10f, 0f);
+            this.vertices[1].Color = Color.Yellow;
+            this.vertices[2].Position = new Vector3(10f, 0f, -5f);
+            this.vertices[2].Color = Color.Green;
         }
     }
 }
